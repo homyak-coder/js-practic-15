6e2a7b5b697d1b7aa53ff7d47f30c578e7b46a0f
xomyak-coder <dkhol@inbox.ru>
2022-02-17 21:01:20 +0300

    Done base 16 homework

--- main.js
+++ main.js
@@ -1,42 +1,18 @@
 "use strict";
 
-const DomElement = function (selector, height, width, bg, fontSize, text) {
-  this.selector = selector;
-  this.height = height;
-  this.width = width;
-  this.bg = bg;
-  this.fontSize = fontSize;
-  this.text = text;
-};
-
-DomElement.prototype.addElements = function () {
-  let addElem;
-  if (this.selector[0] == ".") {
-    addElem = document.createElement("div");
-    addElem.setAttribute("class", this.selector.slice(1));
-    addElem.style.height = this.height;
-    addElem.style.width = this.width;
-    addElem.style.backgroundColor = this.bg;
-    addElem.style.fontSize = this.fontSize;
-    addElem.textContent = this.text;
-  } else if (this.selector[0] == "#") {
-    addElem = document.createElement("p");
-    addElem.setAttribute("id", this.selector.slice(1));
-    addElem.style.height = this.height;
-    addElem.style.width = this.width;
-    addElem.style.backgroundColor = this.bg;
-    addElem.style.fontSize = this.fontSize;
-    addElem.textContent = this.text;
+class First {
+  static hello() {
+    console.log("Привет, я метод родителя!");
   }
-  document.querySelector("body").appendChild(addElem);
-};
+}
 
-const newObject = new DomElement(
-  ".squaer",
-  "100px",
-  "100px",
-  "red",
-  "large",
-  "blbalbalbal"
-);
-newObject.addElements();
+class Second extends First {
+  hello() {
+    First.hello();
+    console.log("А я наследуемый метод!");
+  }
+}
+
+const second = new Second();
+
+second.hello();
